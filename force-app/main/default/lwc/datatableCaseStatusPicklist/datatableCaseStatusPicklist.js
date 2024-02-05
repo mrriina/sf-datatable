14d7d8ce5b98ab23b1fac6da5d60aa01f5a98566
mrriina <mariaip12345678911@gmail.com>
2024-02-05 23:06:52 +0300

    inline editing in lightning-datatable

---
+++ force-app/main/default/lwc/datatableCaseStatusPicklist/datatableCaseStatusPicklist.js
@@ -0,0 +1,38 @@
+import { LightningElement, api } from 'lwc';
+
+export default class DatatableCaseStatusPicklist extends LightningElement {
+    @api label;
+    @api placeholder;
+    @api options;
+    @api value;
+    @api context;
+    @api variant;
+    @api name;
+
+    showPicklist = false;
+
+
+    dispatchCustomEvent(eventName, context, value, label, name) {
+        this.dispatchEvent(new CustomEvent(eventName, {
+            composed: true,
+            bubbles: true,
+            cancelable: true,
+            detail: {
+                data: { context: context, value: value, label: label, name: name }
+            }
+        }));
+    }
+
+
+    handleChange(event) {
+        console.log('in (DatatableCaseStatusPicklist)');
+        event.preventDefault();
+        this.value = event.detail.value;
+        this.showPicklist = false;
+        this.dispatchCustomEvent('valuechange', this.context, this.value, this.label, this.name);
+    }
+
+    handleClick(event) {
+        this.showPicklist = true;
+    }
+}
--- force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
+++ force-app/main/default/lwc/serviceCaseQueueFiltered/serviceCaseQueueFiltered.js
@@ -3,20 +3,86 @@ import { NavigationMixin } from 'lightning/navigation';
 import { refreshApex } from '@salesforce/apex';
 import getUserCases from '@salesforce/apex/ServiceCaseQueueService.getUserCases';
 import getCaseStatusOptions from '@salesforce/apex/ServiceCaseQueueService.getCaseStatusOptions';
+import { updateRecord } from 'lightning/uiRecordApi';
+
+import CASE_OBJECT from '@salesforce/schema/Case';
+import STATUS_FIELD from "@salesforce/schema/Case.Status";
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
 
 
 export default class ServiceCaseQueueFiltered extends NavigationMixin(LightningElement) {
     @track cases = [];
     statusOptions = [];
     isLoading = false;
+    defaultRecordTypeId;
+
     columns = [
         {label: 'Case Number', fieldName: 'CaseNumberUrl', type: 'url', typeAttributes: {label: { fieldName: 'CaseNumber' }, target: '_blank'}},
         { label: 'Assignee', fieldName: 'OwnerId', type: 'text', sortable: true },
-        { label: 'Case Status', fieldName: 'Status', type: 'dropdown', editable: true, typeAttributes: { options: '$statusOptions' } },
+        
         { label: 'Priority', fieldName: 'Priority', type: 'text', sortable: true },
         { label: 'Origin', fieldName: 'Origin', type: 'text', sortable: true },
 
     ];
+
+
+    @wire(getObjectInfo, { objectApiName: CASE_OBJECT })
+    getCaseInfo({data, error}) {
+        if (data) {
+            this.defaultRecordTypeId = data.defaultRecordTypeId;
+            
+        }
+        else if (error) {
+            console.log(error);        
+        }
+    };
+
+
+    @wire(getPicklistValues, { recordTypeId: '$defaultRecordTypeId', fieldApiName: STATUS_FIELD })
+    wiredGetStatusOptions({data, error}){ 
+            if(data){
+                this.statusOptions = data.values;
+                
+                this.columns = [
+                    ...this.columns.slice(0, 2),
+                    {
+                        label: 'Case Status',
+                        fieldName: 'Status',
+                        type: 'picklist',
+                        editable: false,
+                        typeAttributes: {
+                            placeholder: 'Choose Status',
+                            options: this.statusOptions,
+                            value: { fieldName: 'Status' },
+                            context: { fieldName: 'Id' },
+                            variant: 'label-hidden',
+                            name: 'Status',
+                            label: 'Case Status'
+                        }
+                    },
+                    ...this.columns.slice(2)
+                ];
+
+                // this.columns.splice(2,0,{
+                //     label: 'Case Status', fieldName: 'Status', type: 'picklist', editable: false,
+                //         typeAttributes: {
+                //             placeholder: 'Choose Status',
+                //             options: this.statusOptions,
+                //             value: { fieldName: 'Status' },
+                //             context: { fieldName: 'Id' },
+                //             variant: 'label-hidden',
+                //             name: 'Status',
+                //             label: 'Case Status'
+                //         }
+                // });
+
+                
+            }
+            if(error){
+                console.log(error)
+            }
+        }
 
 
     wiredCases;
@@ -24,6 +90,7 @@ export default class ServiceCaseQueueFiltered extends NavigationMixin(LightningE
     wiredGetUserCases(wireResult){ 
         const { data, error } = wireResult;
         this.wiredCases = wireResult;
+        this.isLoading = true;
         if(data){
             this.cases = data.map(record => ({
                 ...record,
@@ -33,22 +100,38 @@ export default class ServiceCaseQueueFiltered extends NavigationMixin(LightningE
         if(error){ 
             console.log(error)
         }
+        this.isLoading = false;
     }
 
-    @wire(getCaseStatusOptions)
-    wiredGetStatusOptions({data, error}){ 
-        if(data){ 
-            this.statusOptions = data;
-        }
-        if(error){ 
-            console.log(error)
-        }
+    // @wire(getCaseStatusOptions)
+    // wiredGetStatusOptions({data, error}){ 
+    //     if(data){ 
+    //         this.statusOptions = data;
+    //         // this.statusOptions = data.map(option => ({ label: option, value: option }));
+    //     }
+    //     if(error){ 
+    //         console.log(error)
+    //     }
+    // }
+
+
+    
+
+    handleValueChange(event) {
+        console.log('in handleValueChange event (ServiceCaseQueueFiltered)');
+        console.log('event.detail.data.value=', event.detail.data.value);
+
+        updatedItem = {
+            Id: event.detail.data.context,
+            Status: event.detail.data.value
+        };
+
     }
 
 
-    handleCellChange(event) {
-        console.log('event.detail=', event.detail);
+    handleSave(event) {
         
+
     }
 
 
